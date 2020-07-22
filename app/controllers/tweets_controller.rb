63ce582c5c8e7382d1e882e2bcd65b784b0d992a
ry <zijoyrb@gamil.com>
2020-07-22 16:09:57 +0900

    最後までコード書いたけど、エラー

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -18,6 +18,13 @@ class TweetsController < ApplicationController
    end
   end
   
+  def search
+    return nil if params[:input] == ""
+    tag = Tag.where(['name LIKE ?', "%#{params[:input]}%"] )
+    render json:{ keyword: tag }
+  end
+   
+
   private
   
    def tweet_params
